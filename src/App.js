24e2ca64fb7f42abd69c517949eaf48c850155c7
gcros <guilhem.cros.38@gmail.com>
2024-06-28 22:59:54 +0200

    .

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import DictWord from './words.txt';
 
 function App() {
+
+  const getDict = () => {
+    let dict; 
+    fetch(DictWord)
+    .then(function(response){
+      return response.text();
+    }).then(function (data) {
+      dict = data.split('\n');
+    })
+    return dict;
+  }
+  const checkWord = (word) => {
+    return getDict().find(word);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
